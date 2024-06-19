b4bec82b985f5eb4456b9f20c8cac408c403b077
Ramen <josueskt32@gmail.com>
2024-06-18 19:03:33 -0500

    asd

--- src/main/java/proyect/toktick/base/usuarios/Usuario.java
+++ src/main/java/proyect/toktick/base/usuarios/Usuario.java
@@ -2,10 +2,8 @@ package proyect.toktick.base.usuarios;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-import java.util.List;
 
 
 import jakarta.persistence.Column;
